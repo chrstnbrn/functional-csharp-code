56c6af0d331cc2e2b9d297c7c7d13f405ce7ece5
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2020-06-10 17:31:04 +0200

    Exercises Chapter 2

---
+++ Exercises/Chapter02/BmiTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Exercises.Chapter2
+{
+    [TestFixture]
+    public class BmiTests
+    {
+        [TestCase(1.8, 80, ExpectedResult = "healthy weight")]
+        [TestCase(1.8, 50, ExpectedResult = "underweight")]
+        [TestCase(1.8, 100, ExpectedResult = "overweight")]
+        [TestCase(1.6, 50, ExpectedResult = "healthy weight")]
+        [TestCase(1.6, 80, ExpectedResult = "overweight")]
+        [TestCase(1.6, 40, ExpectedResult = "underweight")]
+        public string RateWeight_ReturnsExpectedResult(double height, double weight)
+        {
+            return Bmi.RateWeight(height, weight);
+        }
+
+        [TestCase(1.8, 80, ExpectedResult = "healthy weight")]
+        [TestCase(1.8, 50, ExpectedResult = "underweight")]
+        [TestCase(1.8, 100, ExpectedResult = "overweight")]
+        [TestCase(1.6, 50, ExpectedResult = "healthy weight")]
+        [TestCase(1.6, 80, ExpectedResult = "overweight")]
+        [TestCase(1.6, 40, ExpectedResult = "underweight")]
+        public string Run_ReturnsExpectedResult(double height, double weight)
+        {
+            return Bmi.Run(() => height, () => weight);
+        }
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,11 +1,19 @@
-﻿namespace Exercises
+﻿using System;
+
+namespace Exercises
 {
-   public class Program
-   {
-      public static void Main(string[] args)
-      {
-         // run the program you've written, for example:
-         Chapter2.Solutions.Bmi.Run();
-      }
-   }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Func<string> prompt(string promptMessage) => () => { Console.Write(promptMessage); return Console.ReadLine(); };
+            Func<double> promptDouble(string promptMessage) => () => double.Parse(prompt(promptMessage)());
+
+            var result = Chapter2.Bmi.Run(
+                promptDouble("Please enter your height in m: "),
+                promptDouble("Please enter your weight in kg: "));
+
+            Console.WriteLine(result);
+        }
+    }
 }
